acb3d5be1602a68f8da58640ff12f6e7c27cd43f
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-25 17:19:46 -0700

    print_int.c betty fix

--- print_int.c
+++ print_int.c
@@ -12,7 +12,6 @@ int print_int(va_list vl)
 	int counter = 0, len = 0, pt = 1;
 
 	n = va_arg(vl, int);
-
 	if (n == 0)
 	{
 		_putchar('0');
@@ -25,26 +24,21 @@ int print_int(va_list vl)
 			_putchar('-');
 			counter++;
 		}
-
-		N = n; //to not destroy n
+		N = n; /*to not destroy n*/
 		while (N != 0)
 		{
 			N = N / 10;
-			len++; //will get us the N lenght
+			len++; /*will get us the N lenght*/
 		}
-
 		for (i = 1; i < len; i++)
-			pt *= 10; // will use it to get the digits of n
-
+			pt *= 10; /*will use it to get the digits of n*/
 		for (i = 1; i < len + 1; i++)
 		{
 			d = n / pt;
-
 			if (n < 0)
 				_putchar((-1 * d) + 48);
 			else
 				_putchar(d + '0');
-
 			counter++;
 			n -= d * pt;
 			pt = pt / 10;
