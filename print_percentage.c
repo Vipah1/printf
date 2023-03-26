1101d2636bd69271d63c9d94486e252b25fbf3ff
Osman <oselibas@gmail.com>
2023-03-26 15:34:10 -0700

    betty style implemented

--- print_percentage.c
+++ print_percentage.c
@@ -1,14 +1,12 @@
 #include "main.h"
-
 /**
  * print_percentage - prints %%
  *
  * Return: 1
  */
-
 int print_cent(va_list vl)
 {
-  (void)vl;
-  _putchar('%');
-  return (1);
+(void)vl;
+_putchar('%');
+return (1);
 }
