5c0a4c15e00b26f290e25d9ff4ae6f486e96217a
Osman <oselibas@gmail.com>
2023-03-26 14:44:13 -0700

    specifier added

--- check_specifier.c
+++ check_specifier.c
@@ -1,22 +1,25 @@
 #include "main.h"
-
 /**
-* check_specifier - Checks for valid character specifiers and assigns valid function
-* @format: The type of specifier (char*)
-* Return: Returns a pointer to a function, if success
-* NULL pointer if failed
-*/
+ * check_specifier - Checks for a valid character specifier and assigns a valid function.
+ * @format: The type of specifier (char*)
+ * Return: Returns a pointer to a function if success
+ * NULL pointer if failed
+ */
 int (*check_specifier(const char *format))(va_list)
 {
-int i = 0;
-func_s my_array[4] = {{"c", print_char}, {"s", print_str}, {"%", print_cent}, {NULL, NULL}};
-
-for (i = 0; my_array[i].s != NULL; i++)
-{
-if (*(my_array[i].s) == *format)
-{
-return (my_array[i].f);
-}
-}
-return (NULL);
+  int i = 0;
+  func_s my_array[4] = {
+			{"c", printt_char},
+			{"s", print_str},
+			{"%", print_cent},
+			{NULL, NULL}
+  };
+  for (i = 0; my_array[i].s != NULL; i++)
+    {
+      if (*(my_array[i].s == *format)
+	{
+	  return (my_array[i].f);
+	}
+    }
+      return (NULL);
 }
