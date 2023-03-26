9145463814c1edcbb292ec273f5138dc6bdccc6b
Osman <oselibas@gmail.com>
2023-03-26 14:48:26 -0700

    specifier checker corrected

--- check_specifier.c
+++ check_specifier.c
@@ -9,14 +9,14 @@ int (*check_specifier(const char *format))(va_list)
 {
   int i = 0;
   func_s my_array[4] = {
-			{"c", printt_char},
+			{"c", print_char},
 			{"s", print_str},
 			{"%", print_cent},
 			{NULL, NULL}
   };
   for (i = 0; my_array[i].s != NULL; i++)
     {
-      if (*(my_array[i].s == *format)
+      if (*(my_array[i].s) == *format)
 	{
 	  return (my_array[i].f);
 	}
