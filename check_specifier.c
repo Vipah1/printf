1369d3cfbb0ac58a068cd3286aae67ddae22d385
HalimaEla59 <halimaelabbadi@gmail.com>
2023-03-27 10:38:36 -0700

    check_specifier.c updated

--- check_specifier.c
+++ check_specifier.c
@@ -8,13 +8,19 @@
 int (*check_specifier(const char *format))(va_list)
 {
 int i = 0;
-func_s my_array[7] = {
+func_s my_array[13] = {
 {"c", print_char},
 {"s", print_str},
 {"%", print_cent},
 {"d", print_dec},
 {"i", print_int},
-{"b", print_u_i},
+{"b", print_bin},
+{"u", print_u_i},
+{"o", print_oct},
+{"x", print_hex},
+{"X", print_HEX},
+{"p", print_adr},
+{"r", print_rev},
 {NULL, NULL}
 };
 for (i = 0; my_array[i].s != NULL; i++)
