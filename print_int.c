2a71d47c2f63f0873c694f171a099614df514ef0
Osman <oselibas@gmail.com>
2023-03-27 11:00:41 -0700

    print int fix

--- print_int.c
+++ print_int.c
@@ -1,40 +1,34 @@
 #include "main.h"
-
 /**
  * print_int - prints a given integer from variadic parameters
- * @args: variadic parameter
+ * @vl: variadic parameter
  *
  * Return: number of characters printed on Success
  */
-
-int print_int(va_list args)
+int print_int(va_list vl)
 {
-  int i = 0;
-  int count = 0;
-  int numb;
-  int my_arr[10];
-  char x[1];
-
+int i = 0;
+int count = 0;
+int numb;
+int my_arr[10];
+char x[1];
   /*Fetch integer from variadic parameters*/
-  numb = va_arg(args, int);
-
+numb = va_arg(vl, int);
   /*Break the numbers apart and assign them to array*/
-  while (numb != 0)
-    {
-      my_arr[i] = (numb % 10);
-      numb = numb / 10;
-      if (numb == 0)
-	break;
-      else
-	i++;
-    }
-
+while (numb != 0)
+{
+my_arr[i] = (numb % 10);
+numb = numb / 10;
+if (numb == 0)
+break;
+else
+i++;
+}
   /*Write the contents of the array to stdout*/
-  for (i = 0; i >= 0; i--)
-    {
-      x[0] = ('0' + my_arr[i]); /*Convert int to char*/
-      count += write(1, x, 1);
-    }
-
-  return (count);
+for (i = 0; i >= 0; i--)
+{
+x[0] = ('0' + my_arr[i]); /*Convert int to char*/
+count += write(1, x, 1);
+}
+return (count);
 }
