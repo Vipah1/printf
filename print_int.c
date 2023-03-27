f9a356e1736ccc7a8e493201bcdf4cd9bf148dbf
Osman <oselibas@gmail.com>
2023-03-27 10:41:24 -0700

    trying to fix

--- print_int.c
+++ print_int.c
@@ -1,48 +1,37 @@
 #include "main.h"
-
 /**
  * print_int - prints integers
  * @vl: variadic list
  * Return: output lenght
  */
-
 int print_int(va_list vl)
 {
-	int i, d, n, N;
-	int counter = 0, len = 0, pt = 1;
-
-	n = va_arg(vl, int);
-	if (n == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-	else
-	{
-		if (n < 0)
-		{
-			_putchar('-');
-			counter++;
-		}
-		N = n; /*to not destroy n*/
-		while (N != 0)
-		{
-			N = N / 10;
-			len++; /*will get us the N lenght*/
-		}
-		for (i = 1; i < len; i++)
-			pt *= 10; /*will use it to get the digits of n*/
-		for (i = 1; i < len + 1; i++)
-		{
-			d = n / pt;
-			if (n < 0)
-				_putchar((-1 * d) + 48);
-			else
-				_putchar(d + '0');
-			counter++;
-			n -= d * pt;
-			pt = pt / 10;
-		}
-	}
-	return (counter);
+int i = 0;
+int count = 0;
+int numb;
+int my_arr[10];
+char x[1];
+/* Fetch integer from variadic list */
+numb = va_arg(vl, int);
+/* Break numbers apart and assign to array */
+while (numb != 0)
+{
+my_arr[i] = (numb % 10);
+numb = numb / 10;
+if (numb == 0)
+{
+break;
+}
+else
+{
+i++;
+}
+}
+ /* write content of array */
+for (i; i >= 0; i++)
+{
+x[0] = ('0' + my_arr[i]); /* convert int to char*/
+count += write(1, x, 1);
+}
+return (count);
 }
